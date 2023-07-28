78a5db7c38346ac487c986414140d8ace336975f
Mikhail Kostarev <117512786+MKostarev@users.noreply.github.com>
2023-07-28 23:31:42 +0300

    commit

--- task_30.py
+++ task_30.py
@@ -5,6 +5,7 @@ def factorial(n):
     x = 1
     for i in range(1,n + 1):
         x *= i
-    print(x)
+    return x
 
-factorial(int(input('Введите число: ')))
+
+print(factorial(int(input('Введите число: '))))
