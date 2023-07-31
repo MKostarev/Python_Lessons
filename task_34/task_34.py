4f655a781c6d5d285d6ab6eb8fbccac18563eab2
Mikhail Kostarev <117512786+MKostarev@users.noreply.github.com>
2023-08-01 01:13:37 +0300

    commit

--- task_34/task_34.py
+++ task_34/task_34.py
@@ -4,5 +4,8 @@
 
 
 f = open('C:/Работа/Python/Letpy_training/Python_tasks/task_34/task_34.txt')
+#f.write('Важная информация\n')
+#f.write('Вторая по важности информация')
+#f.close()
 content = f.read()
 print(content)
---
+++ task_35/task_35.py
@@ -0,0 +1,14 @@
+catalog = {}
+s = ''
+for i in range(3):
+    n = input()
+    x = int(input())
+    if n in catalog:
+        catalog[n] = catalog[n] + x
+    else:
+        catalog[n] = x
+for k, v in catalog.items():
+    s = s + (k + ':' + str(v)) + '\n'
+    print(k + ':' + str(v))
+with open('task_35.txt', 'w') as f:
+    f.write(s)
