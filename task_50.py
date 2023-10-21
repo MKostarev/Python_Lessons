11176c16819f24ea38d055416f4d7cf6c4fe1cb6
Mikhail Kostarev <117512786+MKostarev@users.noreply.github.com>
2023-10-21 23:48:51 +0300

    commit

---
+++ task_50.py
@@ -0,0 +1,19 @@
+#Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+import codewars_test as test
+from solution import remove_exclamation_marks
+
+
+def remove_exclamation_marks(s):
+    string = s.replace('!','')
+    return string
+
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(remove_exclamation_marks("Hello World!"), "Hello World")
+        test.assert_equals(remove_exclamation_marks("Hello World!!!"), "Hello World")
+        test.assert_equals(remove_exclamation_marks("Hi! Hello!"), "Hi Hello")
+        test.assert_equals(remove_exclamation_marks(""), "")
+        test.assert_equals(remove_exclamation_marks("Oh, no!!!"), "Oh, no")
