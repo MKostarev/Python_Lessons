547b6f7dc61a7af55c49961c11c16a10131892c4
Mikhail Kostarev <117512786+MKostarev@users.noreply.github.com>
2025-01-21 00:20:44 +0400

    commit

--- 57.py
+++ 57.py
@@ -1,9 +1,14 @@
 def square_digits(num):
-  result = []
-  while num > 0:
-    result.append((num % 10)**2)
+    result = []
+    while num > 0:
+        result.append((num % 10) ** 2)
+        num //= 10
+    result.reverse()
+    if result == []:
+        return (0)
+    else:
+        number = int(''.join(map(str, result)))
+        return (number)
 
-    num //= 10
 
-  result.reverse()
-  print(result)
+square_digits(0)
